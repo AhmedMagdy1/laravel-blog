aceaad8979067fd2fbf31e8967267d6736b969c0
TawasolBA <ahmedmag576@gmail.com>
2021-08-30 14:13:55 +0200

    add field to keywords and auto calculate kgr

--- app/Entity/SEO/Keyword.php
+++ app/Entity/SEO/Keyword.php
@@ -6,15 +6,16 @@ use App\Entity\Entity;
 
 class Keyword extends Entity
 {
-    protected $keyword, $search_volume, $kgr, $all_in_title, $keyword_group_id;
+    protected $keyword, $search_volume, $kgr, $all_in_title, $keyword_difficulty, $keyword_group_id;
 
-    public function __construct($keyword, $searchVolume, $kgr, $allInTitle, $keywordGroupId, $id = null)
+    public function __construct($keyword, $searchVolume, $kgr, $allInTitle, $keywordGroupId, $keywordDifficulty, $id = null)
     {
         $this->id = $id;
         $this->keyword = $keyword;
         $this->search_volume = $searchVolume;
         $this->kgr = $kgr;
         $this->all_in_title = $allInTitle;
+        $this->keyword_difficulty = $keywordDifficulty;
         $this->keyword_group_id = $keywordGroupId;
     }
 
@@ -38,4 +39,13 @@ class Keyword extends Entity
         return $this->all_in_title;
     }
 
+    public function getKeywordDifficulty()
+    {
+        return $this->keyword_difficulty;
+    }
+
+    public function getKeywordGroupId()
+    {
+        return $this->keyword_group_id;
+    }
 }
--- resources/views/admin/keyword-groups/create/index.blade.php
+++ resources/views/admin/keyword-groups/create/index.blade.php
@@ -37,20 +37,22 @@
                             <tr>
                                 <th>keyword</th>
                                 <th>Search Volume</th>
-                                <th>KGR</th>
                                 <th>All in title</th>
+                                <th>KGR</th>
+                                <th>Keyword Difficulty</th>
                                 <th>Action</th>
                             </tr>
                             @if($keywordGroupObject->getId() == null)
                                 <tr>
-                                    <td><input name="keywords[0][keyword]" type="text" class="form-control"></td>
-                                    <td><input name="keywords[0][search_volume]" class="search_volume-0 form-control"
+                                    <td><input name="keywords[0][keyword]" index="0" type="text" class="form-control"></td>
+                                    <td><input name="keywords[0][search_volume]" index="0" class="search_volume-0 form-control"
                                                type="text">
                                     </td>
-                                    <td><input name="keywords[0][kgr]" class="kgr-0 form-control" type="text"></td>
-                                    <td><input name="keywords[0][all_in_title]" class="all_in_title-0 form-control"
+                                    <td><input name="keywords[0][all_in_title]" index="0" class="all_in_title-0 form-control"
                                                type="text">
                                     </td>
+                                    <td><input name="keywords[0][kgr]" index="0" class="kgr-0 form-control" type="text" readonly></td>
+                                    <td><input name="keywords[0][keyword_difficulty]" index="0" class="keyword_difficulty-0 form-control" type="text"></td>
                                     <td><a class="btn btn-primary add-new" href="javascript:;"><i
                                                 class="fas fa-plus"></i></a>
                                     </td>
@@ -58,14 +60,15 @@
                             @endif
                             @foreach($keywordGroupObject->getKeywords() as $key => $keyword)
                                 <tr>
-                                    <td><input name="keywords[{{$key}}][keyword]" value="{{$keyword['keyword']}}" type="text" class="form-control"></td>
-                                    <td><input name="keywords[{{$key}}][search_volume]" value="{{$keyword['search_volume']}}" class="search_volume-{{$key}} form-control"
+                                    <td><input name="keywords[{{$key}}][keyword]" index="{{$key}}" value="{{$keyword['keyword']}}" type="text" class="form-control"></td>
+                                    <td><input name="keywords[{{$key}}][search_volume]" index="{{$key}}" value="{{$keyword['search_volume']}}" class="search_volume-{{$key}} form-control"
                                                type="text">
                                     </td>
-                                    <td><input name="keywords[{{$key}}][kgr]"  value="{{$keyword['kgr']}}" class="kgr-{{$key}} form-control" type="text"></td>
-                                    <td><input name="keywords[{{$key}}][all_in_title]"  value="{{$keyword['all_in_title']}}" class="all_in_title-{{$key}} form-control"
+                                    <td><input name="keywords[{{$key}}][all_in_title]" index="{{$key}}"  value="{{$keyword['all_in_title']}}" class="all_in_title-{{$key}} form-control"
                                                type="text">
                                     </td>
+                                    <td><input name="keywords[{{$key}}][kgr]" index="{{$key}}"  value="{{$keyword['kgr']}}" class="kgr-{{$key}} form-control" type="text" readonly></td>
+                                    <td><input name="keywords[{{$key}}][keyword_difficulty]" index="{{$key}}"  value="{{$keyword['keyword_difficulty']}}" class="keyword_difficulty-{{$key}} form-control" type="text"></td>
                                     <td><a class="btn btn-primary add-new" href="javascript:;"><i
                                                 class="fas fa-plus"></i></a>
                                         @if($key > 0)
@@ -102,10 +105,11 @@
         let index = "{{count($keywordGroupObject->getKeywords()) > 0 ? count($keywordGroupObject->getKeywords()) : count($keywordGroupObject->getKeywords())+1}}";
         function addNewLine(lineNumber) {
             $('.keyword-group').append(`<tr>
-                                    <td><input name="keywords[${lineNumber}][keyword]" type="text" class="form-control"></td>
-                                    <td><input name="keywords[${lineNumber}][search_volume]" class="search_volume-${lineNumber} form-control" type="text"></td>
-                                    <td><input name="keywords[${lineNumber}][kgr]" class="kgr-${lineNumber} form-control" type="text"></td>
-                                    <td><input name="keywords[${lineNumber}][all_in_title]" class="all_in_title-${lineNumber} form-control" type="text"></td>
+                                    <td><input name="keywords[${lineNumber}][keyword]" index="${lineNumber}" type="text" class="form-control"></td>
+                                    <td><input name="keywords[${lineNumber}][search_volume]" index="${lineNumber}" class="search_volume-${lineNumber} form-control" type="text"></td>
+                                    <td><input name="keywords[${lineNumber}][all_in_title]" index="${lineNumber}" class="all_in_title-${lineNumber} form-control" type="text"></td>
+                                    <td><input name="keywords[${lineNumber}][kgr]" index="${lineNumber}" class="kgr-${lineNumber} form-control" type="text" readonly></td>
+                                    <td><input name="keywords[${lineNumber}][keyword_difficulty]" index="${lineNumber}" class="keyword_difficulty-${lineNumber} form-control" type="text"></td>
                                     <td>
                                           <a class="btn btn-primary add-new" href="javascript:;"><i class="fas fa-plus"></i></a>
                                           <a class="btn btn-danger remove-line" href="javascript:;"><i class="fas fa-trash"></i></a>
@@ -122,5 +126,12 @@
             event.preventDefault();
             $(this).parents('tr').remove();
         });
+        $(document).on('change', "input[class^='search_volume-'], input[class^='all_in_title-']", function (event) {
+            let index = $(this).attr('index');
+            let searchVolume = $(`.search_volume-${index}`).val();
+            let allInTitle = $(`.all_in_title-${index}`).val();
+            let kgr = (searchVolume > 0) ? allInTitle / searchVolume : 0;
+            $(`.kgr-${index}`).val(kgr.toFixed(2));
+        })
     </script>
 @endsection
